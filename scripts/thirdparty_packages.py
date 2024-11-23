fc2900e1205ec30113c96657874b9f3d0fa021ff
RelativeSure <mail@rasmusj.dk>
2024-11-23 16:35:19 +0100

    Setup 3rd party packages

---
+++ scripts/thirdparty_packages.py
@@ -0,0 +1,73 @@
+#!/bin/env python3
+
+from collections import namedtuple
+
+Git_package = namedtuple(
+    "Package",
+    ["name", "source_dict"],
+)
+
+
+def source_dict(
+    clone_url=None,
+    committish=None,
+    subdirectory=None,
+    specfile=None,
+    source_build_method="rpkg",
+):
+    source_dict_pkg = dict(
+        {
+            "clone_url": clone_url,
+            "committish": committish,
+            "subdirectory": subdirectory,
+            "spec": specfile,
+            "scm_type": "git",
+            "source_build_method": source_build_method,
+        }
+    )
+    return source_dict_pkg
+
+
+def main():
+    thirdparty_packages = {
+        "packages": [
+            Git_package(
+                "rust_tealdeer",
+                source_dict(
+                    clone_url="https://src.fedoraproject.org/rpms/rust-tealdeer",
+                    specfile="rust-tealdeer.spec",
+                ),
+            )._asdict(),
+            Git_package(
+                "wezterm",
+                source_dict(
+                    clone_url="https://github.com/wez/wezterm.git",
+                    source_build_method="make_srpm",
+                ),
+            )._asdict(),
+            Git_package(
+                "zed",
+                source_dict(
+                    clone_url="https://github.com/terrapkg/packages",
+                    subdirectory="/anda/devs/zed/stable",
+                    specfile="zed.spec",
+                ),
+            ),
+            Git_package(
+                "zed-preview",
+                source_dict(
+                    clone_url="https://github.com/terrapkg/packages",
+                    subdirectory="/anda/devs/zed/preview",
+                    specfile="zed-preview.spec",
+                ),
+            ),
+        ]
+    }
+    print(thirdparty_packages["packages"])
+    print(type(thirdparty_packages))
+    print(type(thirdparty_packages["packages"]))
+    return thirdparty_packages
+
+
+if __name__ == "__main__":
+    main()  # This calls your main function
