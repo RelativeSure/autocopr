75b0ad1cdcaa6b790a530d53cd1a43c60cb1d10a
Rasmus <67839982+RelativeSure@users.noreply.github.com>
2024-11-30 00:08:56 +0100

    Update thirdparty_pkgs.py

--- scripts/thirdparty_pkgs.py
+++ scripts/thirdparty_pkgs.py
@@ -2,72 +2,42 @@
 
 from collections import namedtuple
 
-Git_package = namedtuple(
-    "Git_package",
-    ["name", "source_dict"],
-)
+Git_package = namedtuple("Git_package", ["name", "source_dict"])
 
+def source_dict(clone_url=None, committish=None, subdirectory=None, specfile=None, source_build_method="rpkg"):
+    return {
+        "clone_url": clone_url,
+        "committish": committish,
+        "subdirectory": subdirectory,
+        "spec": specfile,
+        "scm_type": "git",
+        "source_build_method": source_build_method,
+    }
 
-def source_dict(
-    clone_url=None,
-    committish=None,
-    subdirectory=None,
-    specfile=None,
-    source_build_method="rpkg",
-):
-    source_dict_pkg = dict(
-        {
-            "clone_url": clone_url,
-            "committish": committish,
-            "subdirectory": subdirectory,
-            "spec": specfile,
-            "scm_type": "git",
-            "source_build_method": source_build_method,
-        }
-    )
-    return source_dict_pkg
-
+def create_git_package(name, clone_url, specfile=None, subdirectory=None, source_build_method="rpkg"):
+    return Git_package(
+        name,
+        source_dict(
+            clone_url=clone_url,
+            specfile=specfile,
+            subdirectory=subdirectory,
+            source_build_method=source_build_method
+        )
+    )._asdict()
 
 def thirdparty_packages_dict():
+    package_definitions = [
+        ("rust_tealdeer", "https://src.fedoraproject.org/rpms/rust-tealdeer", "rust-tealdeer.spec"),
+        ("wezterm", "https://github.com/wez/wezterm.git", None, None, "make_srpm"),
+        ("zed", "https://github.com/terrapkg/packages", "zed.spec", "/anda/devs/zed/stable"),
+        ("zed-preview", "https://github.com/terrapkg/packages", "zed-preview.spec", "/anda/devs/zed/preview")
+    ]
+    
     thirdparty_packages = {
-        "packages": [
-            Git_package(
-                "rust_tealdeer",
-                source_dict(
-                    clone_url="https://src.fedoraproject.org/rpms/rust-tealdeer",
-                    specfile="rust-tealdeer.spec",
-                ),
-            )._asdict(),
-            Git_package(
-                "wezterm",
-                source_dict(
-                    clone_url="https://github.com/wez/wezterm.git",
-                    source_build_method="make_srpm",
-                ),
-            )._asdict(),
-            Git_package(
-                "zed",
-                source_dict(
-                    clone_url="https://github.com/terrapkg/packages",
-                    subdirectory="/anda/devs/zed/stable",
-                    specfile="zed.spec",
-                ),
-            )._asdict(),
-            Git_package(
-                "zed-preview",
-                source_dict(
-                    clone_url="https://github.com/terrapkg/packages",
-                    subdirectory="/anda/devs/zed/preview",
-                    specfile="zed-preview.spec",
-                ),
-            )._asdict(),
-        ]
+        "packages": [create_git_package(*pkg) for pkg in package_definitions]
     }
-    # print(thirdparty_packages["packages"])
-    # print(type(thirdparty_packages))
-    # print(type(thirdparty_packages["packages"]))
+
     return thirdparty_packages
 
-
 if __name__ == "__main__":
-    thirdparty_packages_dict()  # This calls your main function
+    thirdparty_packages_dict()
