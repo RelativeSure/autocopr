9455bb4ab86261544553aa1e31318b94de662342
RelativeSure <67839982+RelativeSure@users.noreply.github.com>
2024-12-26 00:43:04 +0000

    [MegaLinter] Apply linters automatic fixes

--- scripts/copr_thirdparty_packages.py
+++ scripts/copr_thirdparty_packages.py
@@ -1,7 +1,8 @@
 #!/bin/env python3
+from collections import namedtuple
+
 from copr.v3 import Client
 from packages import thirdparty_packages_dict
-from collections import namedtuple
 
 thirdparty_pkgs_array = thirdparty_packages_dict()["packages"]
 
@@ -12,16 +13,19 @@ client = Client.create_from_config_file()
 Package = namedtuple("Git_package", ["name", "source_dict"])
 
 # Convert the dictionary to an array of namedtuples
-thirdparty_pkgs_array = [Package(name=pkg['name'], source_dict=pkg['source_dict']) for pkg in thirdparty_packages_dict()["packages"]]
+thirdparty_pkgs_array = [
+    Package(name=pkg["name"], source_dict=pkg["source_dict"])
+    for pkg in thirdparty_packages_dict()["packages"]
+]
 
 # Print all variables from the namedtuple
 for pkg in thirdparty_pkgs_array:
     name = pkg.name
     source_dict = pkg.source_dict
-    clone_url = pkg.source_dict['clone_url']
-    subdirectory = pkg.source_dict['subdirectory']
-    specfile = pkg.source_dict['spec']
-    source_build_method = pkg.source_dict['source_build_method']
+    clone_url = pkg.source_dict["clone_url"]
+    subdirectory = pkg.source_dict["subdirectory"]
+    specfile = pkg.source_dict["spec"]
+    source_build_method = pkg.source_dict["source_build_method"]
     print(
         f"Name: {name}",
         f"Clone URL: {clone_url}",
@@ -29,7 +33,7 @@ for pkg in thirdparty_pkgs_array:
         f"Specfile: {specfile}",
         f"Source Build Method: {source_build_method}",
         "----------------",
-        sep="\n"
+        sep="\n",
     )
     client.package_proxy.edit(
         "relativesure",  # owner name
