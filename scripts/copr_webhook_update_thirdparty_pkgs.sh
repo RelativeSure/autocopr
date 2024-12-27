1d7e68dcafb5598d1efe494fb8fb40f149b280f2
RelativeSure <mail@rasmusj.dk>
2024-12-27 22:56:55 +0100

    fix script

--- scripts/copr_webhook_update_thirdparty_pkgs.sh
+++ scripts/copr_webhook_update_thirdparty_pkgs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <copr webhook url>"
-  exit 1
-fi
-
 thirdparty_pkgs_array=(act-cli ghostty python-neovim rust-tealdeer utf8proc wezterm zed zed-preview)
 
 for file in "${thirdparty_pkgs_array[@]}"; do
