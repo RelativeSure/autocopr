d41527e1626d0603fc97028f8ca0c69aa4d315ce
Rasmus <67839982+RelativeSure@users.noreply.github.com>
2024-12-28 01:04:10 +0000

    wip

--- scripts/copr_webhook_update_thirdparty_pkgs.sh
+++ scripts/copr_webhook_update_thirdparty_pkgs.sh
@@ -2,7 +2,7 @@
 
 thirdparty_pkgs_array=(act-cli ghostty python-neovim rust-tealdeer utf8proc wezterm zed zed-preview)
 
-for file in "${thirdparty_pkgs_array[@]}"; do
+for file in $thirdparty_pkgs_array; do
   echo "Processing file: $file"
   curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "$COPR_WEBHOOK/$file/"
 done
