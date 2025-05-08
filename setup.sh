1c4d9ee9d348eabd6d1361d0ca8f62ed2a3de2a6
Rasmus <67839982+RelativeSure@users.noreply.github.com>
2025-05-08 17:07:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,18 +31,13 @@ n | N)
   ;;
 esac
 
-# Setup repofile
-repofile="""[copr:copr.fedorainfracloud.org:relativesure:all-packages]
-name=Copr repo for all-packages owned by relativesure
-baseurl=https://download.copr.fedorainfracloud.org/results/relativesure/all-packages/fedora-$releasever-$basearch/
+# Add repository file
+cat > /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:relativesure:all-packages.repo << 'EOF' [copr:copr.fedorainfracloud.org:relativesure:all-packages]
+name=Copr repo for all-packages owned by relativesure baseurl=https://download.copr.fedorainfracloud.org/results/relativesure/all-packages/fedora-$releasever-$basearch/
 type=rpm-md
 skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/relativesure/all-packages/pubkey.gpg
+gpgcheck=1 gpgkey=https://download.copr.fedorainfracloud.org/results/relativesure/all-packages/pubkey.gpg
 repo_gpgcheck=0
 enabled=1
 enabled_metadata=1
-"""
-
-# Add repofile to OS
-echo "$repofile" >/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:relativesure:all-packages.repo
+EOF
